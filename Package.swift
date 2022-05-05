ad4c4d81448ba6a41cd1b39a1073553e80150fd0
Арман Чархчян <re-mont1@mail.ru>
2022-05-05 19:44:15 +0400

    fix

--- Package.swift
+++ Package.swift
@@ -3,9 +3,8 @@
 
 import PackageDescription
 
-private var dependencies: [Package.Dependency] = [.package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0")]
-
-private let remoteDependencies: [Package.Dependency] = [
+private let dependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0"),
     .package(url: "https://github.com/arman095095/Managers.git", branch: "develop"),
     .package(url: "https://github.com/arman095095/Module.git", branch: "develop"),
     .package(url: "https://github.com/arman095095/DesignSystem.git", branch: "develop"),
@@ -15,20 +14,6 @@ private let remoteDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/arman095095/AccountRouteMap.git", branch: "develop"),
     .package(url: "https://github.com/arman095095/UserStoryFacade.git", branch: "develop")
 ]
-
-private let localDependencies: [Package.Dependency] = [
-    .package(path: "../UserStoryFacade"),
-    .package(path: "../Managers"),
-    .package(path: "../Module"),
-    .package(path: "../DesignSystem"),
-    .package(path: "../AlertManager"),
-    .package(path: "../Utils"),
-    .package(path: "../SelectionRouteMap"),
-    .package(path: "../AccountRouteMap")
-]
-
-let isDev = true
-isDev ? dependencies.append(contentsOf: localDependencies) : dependencies.append(contentsOf: remoteDependencies)
 
 let package = Package(
     name: "Account",
